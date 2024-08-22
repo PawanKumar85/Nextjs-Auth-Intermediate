6b4e93c5f5679d069bb2b99707770072b8815640
Pawan Kumar <pawan630703@gmail.com>
2024-08-22 23:18:07 +0530

    Login is added

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,57 +2,77 @@
 import Link from 'next/link'
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import axios from 'axios'
+import toast from 'react-hot-toast';
 
 const Login = () => {
-
+    const router = useRouter();
     const [user, setUser] = useState({
         email: "",
         password: ""
     })
+    const [buttonDisable, setButtonDisable] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
-    const toLogin = async () => { }
+    const toLogin = async () => {
+        try {
+            setLoading(true);
+            await axios.post("/api/users/login", user);
+            toast.success("Login Success");
+            router.push("/profile");
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        const isValid = Object.values(user).every((value) => value.length > 0);
+        setButtonDisable(!isValid);
+    }, [user]);
 
     return (
-        <>
-            <div className="flex flex-col w-[350px] h-fit gap-3 bg-gray-600 p-5 rounded">
-                <h1 className="text-xl font-bold text-center">Log in</h1>
-                <input
-                    type="text"
-                    placeholder="Email"
-                    className="p-2 outline-none text-black rounded"
-                    value={user.email}
-                    onChange={(e) => {
-                        setUser({
-                            ...user,
-                            email: e.target.value
-                        })
-                    }}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="p-2 outline-none text-black rounded lowercase placeholder:capitalize"
-                    value={user.password}
-                    onChange={(e) => {
-                        setUser({
-                            ...user,
-                            password: e.target.value
-                        })
-                    }}
-                />
-                <button className="bg-blue-600 rounded py-2 px-4  hover:bg-blue-700 duration-200">Login</button>
+        <div className="flex flex-col w-[350px] h-fit gap-3 bg-gray-600 p-5 rounded">
+            <h1 className="text-xl font-bold text-center">{
+                loading ? <span className='loader2'></span> : "Log in"
+            }</h1>
+            <input
+                type="text"
+                placeholder="Email"
+                className="p-2 outline-none text-black rounded"
+                value={user.email}
+                onChange={(e) => {
+                    setUser({
+                        ...user,
+                        email: e.target.value
+                    })
+                }}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                className="p-2 outline-none text-black rounded lowercase placeholder:capitalize"
+                value={user.password}
+                onChange={(e) => {
+                    setUser({
+                        ...user,
+                        password: e.target.value
+                    })
+                }}
+            />
+            <button className="bg-blue-600 rounded py-2 px-4 hover:bg-blue-700 duration-200 disabled:opacity-25 shadow-md" disabled={buttonDisable} onClick={toLogin}>Login</button>
 
-                <hr />
-                <Link href={"/signup"}>
-                    <button className="bg-blue-600 rounded py-2 px-4 w-full  hover:bg-blue-700 duration-200">
-                        Sign up
-                    </button>
-                </Link>
-                <p className="flex text-xs capitalize justify-end text-blue-400 hover:underline cursor-pointer">forget password ?</p>
-            </div>
-        </>
+            <hr />
+            <Link href={"/signup"}>
+                <button className="bg-blue-600 rounded py-2 px-4 w-full  hover:bg-blue-700 duration-200">
+                    Sign up
+                </button>
+            </Link>
+            <p className="flex text-xs capitalize justify-end text-blue-400 hover:underline cursor-pointer">forget password ?</p>
+        </div>
     )
 }
 
---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,54 @@
+import connectDB from "@/config/database";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return new Response("User already exists", { status: 400 });
+    }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+
+    if (!validPassword) {
+      return new Response("Invalid password", { status: 400 });
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login Successfully",
+      success: true,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
